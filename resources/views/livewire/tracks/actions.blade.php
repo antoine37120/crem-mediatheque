ebae35f1453ae0918cc866189f444e697257e344
antoine37120 <ant.dupre@gmail.com>
2024-10-16 06:58:33 +0200

    ajout icon svg

--- resources/views/livewire/tracks/actions.blade.php
+++ resources/views/livewire/tracks/actions.blade.php
@@ -1,6 +1,10 @@
 <div>
     <div x-data="trackinfos_{{ $track->id }}">
-        <button x-on:click="addtolist" type="button" class="btn btn-light rounded-circle">+</button>
+        <button x-on:click="addtolist" type="button" class="btn btn-light rounded-circle p-1" style="line-hight:24px;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-lg align-middle display-inline-block" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+            </svg>
+        </button>
     </div>
 </div>
  
