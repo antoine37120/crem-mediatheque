5024ba73e6557601fd93594a9417a7acc064493c
antoine37120 <ant.dupre@gmail.com>
2024-10-21 10:56:16 +0200

    fix issues formulaire options

--- app/Filament/Resources/YearOptionResource/Pages/ListYearOptions.php
+++ app/Filament/Resources/YearOptionResource/Pages/ListYearOptions.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources\YearOptionResource\Pages;
 use App\Filament\Resources\YearOptionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use CactusGalaxy\FilamentAstrotomic\Resources\Pages\Record\ListTranslatable;
 
 class ListYearOptions extends ListRecords
 {
+    use ListTranslatable;
     protected static string $resource = YearOptionResource::class;
 
     protected function getHeaderActions(): array
