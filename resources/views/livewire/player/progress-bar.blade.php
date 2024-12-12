c6f16e72cf6c86090a9aed915039cb6565fbe1e9
vincent_degeorge <vincentdegeorge7@gmail.com>
2024-12-12 13:49:09 +0100

    no message

--- resources/views/livewire/player/progress-bar.blade.php
+++ resources/views/livewire/player/progress-bar.blade.php
@@ -9,7 +9,7 @@
     </script>
     <div class="row align-items-center gx-1">
         <div id="player-progress-time-update" class="col-1 text-end"></div>
-        <div id="player-progress-bar overflow-hidden" class="col-10"></div>
+        <div id="player-progress-bar" class="overflow-hidden col-10"></div>
         <div id="player-progress-duration" class="col-1 text-start"></div>
     </div>
 </div>
