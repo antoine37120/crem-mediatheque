be8c1917406bd634db102535208f4031940d1526
antoine37120 <ant.dupre@gmail.com>
2024-12-13 11:23:43 +0100

    # ajout lien action vers ite archive teaser audio

--- resources/views/livewire/tracks/teaser.blade.php
+++ resources/views/livewire/tracks/teaser.blade.php
@@ -13,6 +13,14 @@
                 <div class="card-track-actions position-absolute top-0 end-0 p-1">
                     <livewire:tracks.actions :track="$track"  wire:key="actions-{{ $track->id }}"/>
                 </div>
+                <div class="card-track-actions position-absolute bottom-0 start-0 p-2">
+                    <a href="{{ $track->link }}" target="_blank"class="btn btn-light rounded-circle p-0 d-flex align-items-center justify-content-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
 
@@ -37,6 +45,14 @@
                 </div>
                 <div class="card-track-actions position-absolute top-0 end-0 p-1">
                     <livewire:tracks.actions :track="$track"  wire:key="actions-{{ $track->id }}"/>
+                </div>
+                <div class="card-track-actions position-absolute bottom-0 start-0 p-2">
+                    <a href="{{ $track->link }}" target="_blank"class="btn btn-light rounded-circle p-0 d-flex align-items-center justify-content-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
