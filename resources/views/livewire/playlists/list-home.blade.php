4f0fa28d9c3e618109faf09560fe753915e3009d
vincent_degeorge <vincentdegeorge7@gmail.com>
2025-03-10 13:37:33 +0100

    mobile : home / playlists & podcasts : img size & horizontal scroll

--- resources/views/livewire/playlists/list-home.blade.php
+++ resources/views/livewire/playlists/list-home.blade.php
@@ -2,9 +2,10 @@
     <h2 class="px-4 pb-4 fw-bold">
         <a href="{{route('playlists')}}" class="text-black text-decoration-none" wire:navigate>Playlists</a>
     </h2>
-    <div class="row align-items-start g-5 pb-5">
+    <div class="row flex-nowrap align-items-start g-5 pb-5">
     @foreach ($playlists as $playlist)
-        <div class="col-sm-6 col-lg-4 col-xxl-3  px-5">
+        <div class="col-3 px-5 me-5 me-sm-3 me-lg-0">
+            {{-- col-sm-6 col-lg-4 col-xxl-3 --}}
             <livewire:playlists.teaser :playlist="$playlist" key="home-playlist-teaser-{{$playlist}}" />
         </div>
     @endforeach
--- resources/views/livewire/podcasts/list-home.blade.php
+++ resources/views/livewire/podcasts/list-home.blade.php
@@ -1,8 +1,9 @@
 <div>
     <h2 class="px-4 pb-4 fw-bold"><a href="{{route('podcasts')}}" class="text-black text-decoration-none" wire:navigate>Podcasts</a></h2>
-    <div class="row align-items-start g-5 pb-5">
+    <div class="row flex-nowrap align-items-start g-5 pb-5">
     @foreach ($podcasts as $podcast)
-        <div class="col-sm-6 col-lg-4 col-xxl-3  ps-5">
+        <div class="col-3 ps-5 me-5 me-sm-3 me-lg-0">
+            {{-- col-sm-6 col-lg-4 col-xxl-3 --}}
         <livewire:podcasts.teaser :podcast="$podcast" />
         </div>
     @endforeach
