e699f89346ee14a86f882ed677a5438538c0458e
vincent_degeorge <vincentdegeorge7@gmail.com>
2025-03-13 15:12:38 +0100

    pages podcasts & playlists : alignements & marges responsive

--- resources/views/livewire/podcasts/list-self.blade.php
+++ resources/views/livewire/podcasts/list-self.blade.php
@@ -2,9 +2,9 @@
     {{-- page liste des podcasts --}}
 
     @foreach ($podcasts as $podcast)
-    <div class="container my-4">
-        <div class="row align-items-start my-2 gy-5">
-            <div  class="col-2 position-relative p-0 ps-2 mt-2 offset-1">
+    <div class="container podcasts-list-self my-4">
+        <div class="row flex-nowrap align-items-start my-2 gy-5">
+            <div  class="col-2 position-relative p-0 ps-2 mt-2 offset-lg-1">
                 <div class="position-relative d-inline-block p-0 m-0">
                     <img src="{{ url('storage/'.$podcast->picture) }}" class="card-img-top rounded border border-dark" alt="..."/>
                     <div class="card-playlist-actions position-absolute top-0 end-0 p-1">
@@ -13,7 +13,6 @@
                 </div>
             </div>
             <div class="col-10 col-md-9 mt-2 offset-1">
-                {{-- <h5>Titre du podcast 1</h5> --}}
                 <h5 class="card-title"><a  href="{{route('podcast', ['podcast' => $podcast->id])}}" class="text-black text-decoration-none fw-bold" wire:navigate>{{ Illuminate\Support\Str::limit($podcast->translate(App::getLocale(), true)->name, 45, '...') }}</a></h5>
                 <p>{!! $podcast->translate(App::getLocale(), true)->description !!}</p>
             </div>
