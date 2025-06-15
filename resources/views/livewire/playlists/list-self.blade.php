abb288f4eea4543f5d6267f5fdfd5997510ab466
antoine37120 <ant.dupre@gmail.com>
2025-06-15 13:42:04 +0200

    fix offset null creat overflow

--- resources/views/livewire/playlists/list-self.blade.php
+++ resources/views/livewire/playlists/list-self.blade.php
@@ -4,7 +4,7 @@
         @foreach ($playlists as $playlist)
         <div class="container playlists-list-self my-4">
             <div class="row flex-nowrap align-items-start my-2 gy-5">
-                <div class="col-2 position-relative p-0 ps-2 mt-2 offset-lg-1">
+                <div class="col-auto position-relative p-0 ps-2 mt-2">
                     {{-- style="width: fit-content" --}}
                     <div class="position-relative d-inline-block p-0 m-0">
                         <img src="{{ url('storage/'.$playlist->picture) }}" class="card-img-top rounded border border-dark" alt="..."/>
@@ -13,7 +13,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-10 col-md-9 mt-2 offset-1">
+                <div class="col-auto mt-2">
                     <h5 class="card-title"><a  href="{{route('playlist', ['playlist' => $playlist->id])}}" class="text-black text-decoration-none fw-bold" wire:navigate>{{ Illuminate\Support\Str::limit($playlist->translate(App::getLocale(), true)->name, 45, '...') }}</a></h5>
                     <p>{!! $playlist->translate(App::getLocale(), true)->description !!}</p>
                 </div>
--- resources/views/livewire/podcasts/list-self.blade.php
+++ resources/views/livewire/podcasts/list-self.blade.php
@@ -4,7 +4,7 @@
     @foreach ($podcasts as $podcast)
     <div class="container podcasts-list-self my-4">
         <div class="row flex-nowrap align-items-start my-2 gy-5">
-            <div  class="col-2 position-relative p-0 ps-2 mt-2 offset-lg-1">
+            <div  class="col-auto position-relative p-0 ps-2 mt-2">
                 <div class="position-relative d-inline-block p-0 m-0">
                     <img src="{{ url('storage/'.$podcast->picture) }}" class="card-img-top rounded border border-dark" alt="..."/>
                     <div class="card-playlist-actions position-absolute top-0 end-0 p-1">
@@ -12,7 +12,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-10 col-md-9 mt-2 offset-1">
+            <div class="col-auto mt-2">
                 <h5 class="card-title"><a  href="{{route('podcast', ['podcast' => $podcast->id])}}" class="text-black text-decoration-none fw-bold" wire:navigate>{{ Illuminate\Support\Str::limit($podcast->translate(App::getLocale(), true)->name, 45, '...') }}</a></h5>
                 <p>{!! $podcast->translate(App::getLocale(), true)->description !!}</p>
             </div>
