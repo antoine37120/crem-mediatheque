51b8a9649c50b9e3f16ead3908cbbbffdaf4475d
antoine37120 <ant.dupre@gmail.com>
2025-12-04 18:36:30 +0100

    fix mobil playlist full img

--- resources/views/livewire/podcasts/full-podcast.blade.php
+++ resources/views/livewire/podcasts/full-podcast.blade.php
@@ -6,12 +6,12 @@
         <div class="col-12 offset-lg-1 col-lg-11 p-2 px-lg-0 pt-lg-0 pb-lg-4">
             <div class="row align-items-start m-0">
 
-                <div class="col-6 offset-3 offset-lg-0 col-lg-3 col-xxl-2 d-flex justify-content-sm-start px-0 d-lg-none">
-                    <div class="position-relative col-10 col-lg-12 mx-auto">
+                <div class="col-6 col-sm-6 offset-sm-3 offset-lg-0 col-lg-3 col-xxl-2 d-flex justify-content-sm-start px-0 d-lg-none">
+                    <div class="position-relative col-10 col-lg-12 mx-0 mx-sm-auto">
                         <img src="{{ url('storage/'.$podcast->picture) }}" class="card-img-top rounded border border-dark border-2" style="" alt="..." width="150" height="150"/>
                     </div>
                 </div>
-                <div class="col-3 col-lg-12 align-items-end px-lg-4 pb-lg-4">
+                <div class="col-6 col-sm-3 col-lg-12 align-items-end px-lg-4 pb-lg-4">
                     <livewire:playlists.full-actions :playlist="$podcast" wire:key="playlist-full-actions-{{ $podcast->id }}" />
                 </div>
                 <div class="col-12 d-lg-none my-4">
